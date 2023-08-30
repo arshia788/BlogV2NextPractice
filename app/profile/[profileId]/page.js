de6e086518104bbda13d3586f73b80a777291348
Amirali Amanipour <arshia.ala80@gmail.com>
2023-08-30 18:23:44 +0330

    finished the profile component

---
+++ app/profile/[profileId]/page.js
@@ -0,0 +1,109 @@
+import ProfileCard from 'components/module/shared/ProfileCard'
+import Image from 'next/image'
+import React from 'react'
+
+export default function ProfileDetail() {
+
+    const data = {
+        image: '/images/ads/business_social_media_banner_19.jpg',
+        name: 'Blog name',
+        short_desc: 'title of the blog is something that I...',
+        followers_number: 20,
+        following_number: 5,
+        posts: [
+            {
+                image: '/images/posts/tablet1.jpg',
+                title: 'title of the blog',
+                short_desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque  ',
+                comments_number: 10,
+                view_number: 120,
+                slug: 'tablet1',
+                date: '1 December 1999'
+            }
+            ,
+            {
+                image: '/images/posts/tablet2.jpg',
+                title: 'title of the blog',
+                short_desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque  ',
+                comments_number: 22,
+                view_number: 502,
+                slug: 'mobile1',
+                date: '1 December 1999'
+            }
+            ,
+            {
+                image: '/images/posts/tablet3.jpg',
+                title: 'title of the blog',
+                short_desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque',
+                comments_number: 25,
+                view_number: 12004,
+                slug: 'tablet1',
+                date: '1 December 1999'
+            },
+
+            {
+                image: '/images/posts/tablet1.jpg',
+                title: 'title of the blog',
+                short_desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque  ',
+                comments_number: 10,
+                view_number: 120,
+                slug: 'laptop1',
+                date: '1 December 1999'
+            },
+            {
+                image: '/images/posts/tablet1.jpg',
+                title: 'title of the blog',
+                short_desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque  ',
+                comments_number: 10,
+                view_number: 120,
+                slug: 'laptop1',
+                date: '1 December 1999'
+            },
+            {
+                image: '/images/posts/tablet1.jpg',
+                title: 'title of the blog',
+                short_desc: 'Lorem ipsum dolor sit amet, asdawd ',
+                comments_number: 10,
+                view_number: 120,
+                slug: 'laptop1',
+                date: '1 December 1999'
+            },
+
+        ],
+    }
+    return (
+        <div className='flex flex-col gap-y-8'>
+            <section className='flex flex-col gap-4 items-center mt-4'>
+
+                <div className='relative w-[130px] h-[130px] rounded-full overflow-hidden'>
+                    <Image src={data.image} fill alt={data.title} className='object-cover'/>
+                </div>
+
+                <p>{data.name}</p>
+                <p>{data.short_desc}</p>
+
+                <div className='flex justify-between items-center gap-4'>
+                    <p>follows: {data.followers_number}</p>
+                    <p>following: {data.following_number}</p>
+                </div>
+
+                <button className='bg-blue-700 rounded px-10 py-2 text-white'>Follow</button>
+                
+
+
+            </section>
+
+            <section className='p-4'>
+                <h2 className='text-lg font-medium mb-4'>My Blogs</h2>
+
+                <div className='flex flex-col '>
+                    {
+                        data.posts.map((item, index) => <ProfileCard key={index} data={item} />)
+                    }
+                </div>
+
+            </section>
+
+        </div>
+    )
+}
---
+++ components/module/shared/ProfileCard.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ProfileCard({data}) {
+
+  return (
+    <article className="shadow-[0px_10px_10px_rgba(0,0,0,0.25)] rounded-md flex justify-start items-center gap-2 h-[250px] p-4 mb-8">
+        
+        {/* image left */}
+        <div className="relative w-[300px] min-w-[300px] h-[180px] rounded overflow-hidden "> 
+            <Image src={data.image} alt={data.title} fill className="object-cover"/>
+        </div>
+
+        {/* info right. */}
+        <div className="flex flex-col gap-4">
+            <div className="flex justify-between items-center">
+                <h3 className="font-semibold">{data.title}</h3>
+                <p className="bg-blue-700 px-3 py-1 rounded text-white">{data.date}</p>
+            </div>
+
+            <p className="w-full h-[100px] line-clamp-1 text-zinc-500">{data.short_desc}</p>
+
+            <div className="flex justify-between items-center ">
+
+                <div className="flex justify-between items-center gap-4">
+                    <p className="text-blue-700">comments:<span className="text-black">{data.comments_number}</span></p>
+                    <p className="text-blue-700">views: <span className="text-black">{data.view_number}</span> </p>
+                </div>
+
+                <Link 
+                className="bg-blue-700 rounded text-white px-3 py-1"
+                href={data.slug}>
+                    continue
+                </Link>
+            </div>
+        </div>
+
+    </article>
+  )
+}
